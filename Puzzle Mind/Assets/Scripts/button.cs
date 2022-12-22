0a7870f0d8755cf5c329f1ef510805065e85b6e3
LightningFz <katfawaz64@gmail.com>
2022-12-22 14:31:56 -0500

    door and the button

--- Puzzle Mind/Assets/Scripts/button.cs
+++ Puzzle Mind/Assets/Scripts/button.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Button : MonoBehaviour
 {
     door dr;
-    public void OnTriggerEnter2D(Collision2D other)
+    public void OnTriggerEnter2D(Collider2D other)
     {
         dr = gameObject.GetComponent<door>();
         if(other.transform.name == "Charecter1"){
